51b11179d7c9dda153c26f076880644aabac0366
fadelanumah <fadelanumah@gmail.com>
2024-01-24 17:57:14 +0700

    feat: Add Base Layout Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,25 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
 const layout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className="main">
-                <div />
-            </div>
+      <body>
+        <div className="main">
+          <div />
+        </div>
 
-            <main className="app">
-                {children}
-            </main>
-        </body>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
     </html>
   );
 };
